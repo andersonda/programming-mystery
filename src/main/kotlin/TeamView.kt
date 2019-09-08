9eb7b389296a7ecc31e0db620ceb9eba82b200ed
David Anderson <andersonda8109@gmail.com>
2019-09-07 19:45:45 -0700

    increased size of input font

--- src/main/kotlin/TeamView.kt
+++ src/main/kotlin/TeamView.kt
@@ -33,14 +33,13 @@ class TeamView : View() {
         teams = newTeams.map { Team(it, 0) }
 
         teams.forEachIndexed {index, team ->
-            val text = Text(team.name)
-            text.font = Font(16.0)
+            val font = Font(18.0)
             val image = ImageView("question-mark.png")
-            image.fitWidth = 16.0
-            image.fitHeight = 16.0
+            image.fitWidth = 18.0
+            image.fitHeight = 18.0
 
-            root.add(text, 0, index)
-            root.add(TextField(), 1, index)
+            root.add(Text(team.name).apply { this.font = font }, 0, index)
+            root.add(TextField().apply{ this.font = font }, 1, index)
             root.add(image, 2, index)
         }
     }
